528affc699d1aa44627c376ed41f866319c0a84e
Ikevine <ishimwekevine098@gmail.com>
2024-06-06 08:32:36 +0200

    pages & routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes , Route} from "react-router-dom"
+import About from './pages/about';
+import Home from './pages/home';
+import SignIn from "./pages/signIn";
+import Profile from "./pages/profile";
+import SignUp from './pages/signUp';
 function App() {
-
-
   return (
-    <>
-       <h1 className="">Hellow people</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/about" element={<About/>}/>
+         <Route path="/sign-in" element={<SignIn/>}/>
+         <Route path="/profile" element={<Profile/>}/>
+         <Route path="/sign-up" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
